99a8cebf7edd0f6fb148e70a561a2e154094a1ec
Ross Thompson <ross.thompson@statcan.gc.ca>
2014-09-16 14:53:07 -0400

    Update drupal command to load Virtual Library datasets into the opendata_package table in the Drupal database.

--- ckanext/drupal/commands.py
+++ ckanext/drupal/commands.py
@@ -1,8 +1,9 @@
-from ckan import model
+
 from ckan.lib.cli import CkanCommand
 from ckan.lib.cli import parse_db_config
 import logging
 import psycopg2
+import json
 import sys
 
 
@@ -13,6 +14,7 @@ class DrupalCommand(CkanCommand):
     Usage::
 
        paster drupal load-packages [-c <path to config file>]
+                     load-docs [-c <path to config file>]
     """
     summary = __doc__.split('\n')[0]
     usage = __doc__
@@ -35,7 +37,11 @@ class DrupalCommand(CkanCommand):
                 self.load_packages()
             except KeyboardInterrupt:
                 pass
-
+        elif cmd == 'load-docs':
+            try:
+                self.load_docs()
+            except KeyboardInterrupt:
+                pass
         else:
             print self.__doc__
 
@@ -97,6 +103,71 @@ class DrupalCommand(CkanCommand):
 
         ckan_cursor.close()
         ckan_conn.close()
+
+    def load_docs(self):
+        '''
+        Load the Virtual Library datasets into the same Drupal table as the Open Data datasets.
+        @return: nothing
+        '''
+
+        #Get our CKAN and Drupal connection string
+
+        dbc = parse_db_config('sqlalchemy.url')
+        ckan_conn_string = "host='%s' dbname='%s' user='%s' password='%s'" % (dbc['db_host'], dbc['db_name'], dbc['db_user'], dbc['db_pass'])
+
+        dbd = parse_db_config('ckan.drupal.url')
+        drupal_conn_string = "host='%s' dbname='%s' user='%s' password='%s'" % (dbd['db_host'], dbd['db_name'], dbd['db_user'], dbd['db_pass'])
+
+        # get a connection, if a connect cannot be made an exception will be raised here
+        ckan_conn = psycopg2.connect(ckan_conn_string)
+        drupal_conn = psycopg2.connect(drupal_conn_string)
+
+        # ckan_conn.ckan_cursor will return a ckan_cursor object, you can use this ckan_cursor to perform queries
+        ckan_cursor = ckan_conn.cursor()
+        drupal_cursor = drupal_conn.cursor()
+
+        # execute our Query
+        ckan_cursor.execute("""select p.id,
+       p.name,
+       case when pe1.value is null then '' else pe1.value end,
+       case when pe2.value is null then '' else pe2.value end
+      from package p
+      left join package_extra pe1 on p.id = pe1.package_id and pe1.key = 'title_ml'
+      left join package_extra pe2 on p.id = pe2.package_id and pe2.key = 'description_ml'
+      where p.type = 'doc' and p.state = 'active'""")
+
+        # retrieve the records from the CKAN database and insert into the Drupal database
+        for rec in ckan_cursor:
+            drupal_cursor.execute("""select count(*) from opendata_package where pkg_id = %s""", (rec[0],))
+            row = drupal_cursor.fetchone()
+            if row[0] == 0:
+                titles = json.loads(rec[2])
+                descriptions = json.loads(rec[3])
+                print "Inserting package %s: %s %s %s: %s %s" % (rec[0], rec[1], titles['en'], titles['fr'],
+                                                       descriptions['en'], descriptions['fr'])
+                try:
+                    drupal_cursor.execute("""insert into opendata_package (
+  pkg_id,
+  pkg_name,
+  pkg_title_en,
+  pkg_title_fr,
+  pkg_description_en,
+  pkg_description_fr
+) values (%s, %s, %s, %s, %s, %s)""", (rec[0], rec[1], titles['en'], titles['fr'],
+                                                       descriptions['en'], descriptions['fr']))
+
+                except psycopg2.DataError, e:
+                    self.logger.warn('Postgresql Database Exception %s', e.message)
+
+        # Close the connections
+
+        drupal_conn.commit()
+        drupal_cursor.close()
+        drupal_conn.close()
+
+        ckan_cursor.close()
+        ckan_conn.close()
+
 
     def format_drupal_string(self, ds):
         dstr = ''
